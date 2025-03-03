abb6dc909782e15fdc50d273c5b1d481a40983e8
marcofong <marco.fong@gmail.com>
2025-03-03 20:47:41 +0100

    More users added, the AccountController is using a DTO created.

--- DatingAppCourse.API/Controllers/AccountController.cs
+++ DatingAppCourse.API/Controllers/AccountController.cs
@@ -1,22 +1,26 @@
 using System.Security.Cryptography;
 using System.Text;
 using DatingAppCourse.API.Data;
+using DatingAppCourse.API.DTOs;
 using DatingAppCourse.API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingAppCourse.API.Controllers;
 
 public class AccountController(DataContext context) : BaseApiController
 {
     [HttpPost("register")] //account/register
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            Username = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            Username = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -24,5 +28,11 @@ public class AccountController(DataContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => 
+            x.Username.ToLower() == username.ToLower());
     }
 }
